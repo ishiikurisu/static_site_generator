cbc0adb754d0574e0d113778bce5b528d4018ba3
Cris Silva Jr. <crisjr@pm.me>
2025-06-22 17:05:50 +0100

    rename production build binaries

--- cross-compile.sh
+++ cross-compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
-env GOOS=android GOARCH=arm64 go build -o build_notes.android.arm64.exe main/main.go 
-env GOOS=darwin GOARCH=amd64 go build -o build_notes.darwin.amd64.exe main/main.go 
-env GOOS=linux GOARCH=amd64 go build -o build_notes.linux.amd64.exe main/main.go 
+env GOOS=linux GOARCH=arm64 go build -ldflags "-s -w" -o build_notes.linux.aarch64.exe main/main.go 
+env GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o build_notes.linux.x86_64.exe main/main.go 
+env GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o build_notes.darwin.x86_64.exe main/main.go 
 
