8285af5dc3aa3516441e05208af727fac6d36756
Cris Silva Jr. <crisjr@pm.me>
2025-12-07 23:34:31 +0000

    lein new app br.eng.crisjr.static_site_generator

--- cross-compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -ex
-env GOOS=linux GOARCH=arm64 go build -ldflags "-s -w" -o build_notes.linux.aarch64.exe main/main.go 
-env GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o build_notes.linux.x86_64.exe main/main.go 
-env GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o build_notes.darwin.x86_64.exe main/main.go 
-
--- go.mod
+++
@@ -1,8 +0,0 @@
-module gitlab.com/crisjr/static_site_generator
-
-go 1.23.2
-
-require (
-	git.sr.ht/~m15o/gmi2html v0.4.0 // indirect
-	github.com/gomarkdown/markdown v0.0.0-20240930133441-72d49d9543d8 // indirect
-)
